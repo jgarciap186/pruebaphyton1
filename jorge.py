6edb741f92e6daf03147cd8a609a585a65d039bc
jgarciap186 <jorgegarciap@outlook.com>
2020-08-17 17:40:38 -0600

    cmbio1

--- jorge.py
+++ jorge.py
@@ -2,3 +2,10 @@ from datetime import timedelta, date
 
 print(timedelta(minutes=100))
 print (date.today())
+
+print(timedelta(minutes=100))
+print (date.today())
+
+
+print(timedelta(minutes=100))
+print (date.today())
