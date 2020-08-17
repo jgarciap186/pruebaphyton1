79788574fccff4eb114ff1e2739a9232520da3ba
jgarciap186 <jorgegarciap@outlook.com>
2020-08-17 17:43:41 -0600

    modifique nuevamente

--- jorge.py
+++ jorge.py
@@ -5,7 +5,8 @@ print (date.today())
 
 print(timedelta(minutes=100))
 print (date.today())
-
+print(timedelta(minutes=100))
+print (date.today())
 
 print(timedelta(minutes=100))
 print (date.today())
